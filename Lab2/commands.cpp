65aac0782d0e31d8c2214bc2ea37e3c9fec9d948
Max <mostarr@ucsc.edu>
2020-01-29 21:08:51 -0800

    I hate pointers

--- Lab2/commands.cpp
+++ Lab2/commands.cpp
@@ -15,20 +15,21 @@ command_hash cmd_hash{
     {"prompt", fn_prompt},
     {"pwd", fn_pwd},
     {"rm", fn_rm},
+    {"rmr", fn_rmr},
 };
 
 command_fn find_command_fn(const string &cmd)
 {
-  // Note: value_type is pair<const key_type, mapped_type>
-  // So: iterator->first is key_type (string)
-  // So: iterator->second is mapped_type (command_fn)
-  DEBUGF('c', "[" << cmd << "]");
-  const auto result = cmd_hash.find(cmd);
-  if (result == cmd_hash.end())
-  {
-    throw command_error(cmd + ": no such function");
-  }
-  return result->second;
+    // Note: value_type is pair<const key_type, mapped_type>
+    // So: iterator->first is key_type (string)
+    // So: iterator->second is mapped_type (command_fn)
+    DEBUGF('c', "[" << cmd << "]");
+    const auto result = cmd_hash.find(cmd);
+    if (result == cmd_hash.end())
+    {
+        throw command_error(cmd + ": no such function");
+    }
+    return result->second;
 }
 
 command_error::command_error(const string &what) : runtime_error(what)
@@ -37,159 +38,175 @@ command_error::command_error(const string &what) : runtime_error(what)
 
 int exit_status_message()
 {
-  int status = exec::status();
-  cout << exec::execname() << ": exit(" << status << ")" << endl;
-  return status;
+    int status = exec::status();
+    cout << exec::execname() << ": exit(" << status << ")" << endl;
+    return status;
 }
 
 void fn_cat(inode_state &state, const wordvec &words)
 {
-  DEBUGF('c', state);
-  DEBUGF('c', words);
-  auto cwdPtr = state.cwd();
-  auto cwdCts = cwdPtr->getContents();
-  inode_ptr file = cwdCts->getDirent(words.at(1));
-  try
-  {
-    wordvec fileContents = file->getContents()->readfile();
-    if (fileContents.size() > 0)
+    DEBUGF('c', state);
+    DEBUGF('c', words);
+    auto cwdPtr = state.cwd();
+    auto cwdCts = cwdPtr->getContents();
+    inode_ptr file = cwdCts->getDirent(words.at(1));
+    try
     {
-      string fileString = "";
-      for (string word : fileContents)
-      {
-        fileString += word;
-        if (word != words.back())
+        wordvec fileContents = file->getContents()->readfile();
+        if (fileContents.size() > 0)
         {
-          fileString += " ";
+            string fileString = "";
+            for (string word : fileContents)
+            {
+                fileString += word;
+                if (word != words.back())
+                {
+                    fileString += " ";
+                }
+            }
+            cout << fileString << endl;
         }
-      }
-      cout << fileString << endl;
     }
-  }
-  catch (file_error &)
-  {
-    cout << "cat: " << words.at(1) << ": No such file or directory" << endl;
-    exec::status(1);
-  }
+    catch (file_error &)
+    {
+        cout << "cat: " << words.at(1) << ": No such file or directory" << endl;
+        exec::status(1);
+    }
 }
 
 void fn_cd(inode_state &state, const wordvec &words)
 {
-  DEBUGF('c', state);
-  DEBUGF('c', words);
-  auto cwdPtr = state.cwd();
-  auto cwdCts = cwdPtr->getContents();
-  inode_ptr newCwd = cwdCts->getDirent(words.at(1));
-  state.cwd(newCwd);
+    DEBUGF('c', state);
+    DEBUGF('c', words);
+    auto cwdPtr = state.cwd();
+    auto cwdCts = cwdPtr->getContents();
+    inode_ptr newCwd = cwdCts->getDirent(words.at(1));
+    state.cwd(newCwd);
 }
 
 void fn_echo(inode_state &state, const wordvec &words)
 {
-  DEBUGF('c', state);
-  DEBUGF('c', words);
-  cout << word_range(words.cbegin() + 1, words.cend()) << endl;
+    DEBUGF('c', state);
+    DEBUGF('c', words);
+    cout << word_range(words.cbegin() + 1, words.cend()) << endl;
 }
 
 void fn_exit(inode_state &state, const wordvec &words)
 {
-  DEBUGF('c', state);
-  DEBUGF('c', words);
-  throw ysh_exit();
+    DEBUGF('c', state);
+    DEBUGF('c', words);
+    throw ysh_exit();
 }
 
 void fn_ls(inode_state &state, const wordvec &words)
 {
-  DEBUGF('c', state);
-  DEBUGF('c', words);
-  auto cwdPtr = state.cwd();
-  auto cwdCts = cwdPtr->getContents();
-  cwdCts->ls();
+    DEBUGF('c', state);
+    DEBUGF('c', words);
+    auto cwdPtr = state.cwd();
+    auto cwdCts = cwdPtr->getContents();
+    cwdCts->ls();
 }
 
 void fn_lsr(inode_state &state, const wordvec &words)
 {
-  DEBUGF('c', state);
-  DEBUGF('c', words);
-  auto cwdPtr = state.cwd();
-  auto cwdCts = cwdPtr->getContents();
-  cwdCts->lsr();
+    DEBUGF('c', state);
+    DEBUGF('c', words);
+    auto cwdPtr = state.cwd();
+    auto cwdCts = cwdPtr->getContents();
+    cwdCts->lsr();
 }
 
 void fn_make(inode_state &state, const wordvec &words)
 {
-  DEBUGF('c', state);
-  DEBUGF('c', words);
-  if (words.size() > 2)
-  {
-    wordvec sub(words.begin() + 2, words.end());
-    state.cwd()->getContents()->mkfile(words.at(1))->getContents()->writefile(sub);
-  }
+    DEBUGF('c', state);
+    DEBUGF('c', words);
+    if (words.size() > 2)
+    {
+        wordvec sub(words.begin() + 2, words.end());
+        state.cwd()->getContents()->mkfile(words.at(1))->getContents()->writefile(sub);
+    }
 }
 
 void fn_mkdir(inode_state &state, const wordvec &words)
 {
-  DEBUGF('c', state);
-  DEBUGF('c', words);
-  if (words.size() > 1)
-  {
-    string name = "";
-    wordvec sub(words.begin() + 1, words.end());
-    for (string word : sub)
+    DEBUGF('c', state);
+    DEBUGF('c', words);
+    if (words.size() > 1)
     {
-      name += word;
-      if (word != words.back())
-      {
-        name += " ";
-      }
+        string name = "";
+        wordvec sub(words.begin() + 1, words.end());
+        for (string word : sub)
+        {
+            name += word;
+            if (word != words.back())
+            {
+                name += " ";
+            }
+        }
+        state.cwd()->getContents()->mkdir(name);
     }
-    state.cwd()->getContents()->mkdir(name);
-  }
 }
 
 void fn_prompt(inode_state &state, const wordvec &words)
 {
-  DEBUGF('c', state);
-  DEBUGF('c', words);
-  if (words.size() > 1)
-  {
-    string prompt = "";
-    wordvec sub(words.begin() + 1, words.end());
-    for (string word : sub)
+    DEBUGF('c', state);
+    DEBUGF('c', words);
+    if (words.size() > 1)
     {
-      prompt += word + " ";
+        string prompt = "";
+        wordvec sub(words.begin() + 1, words.end());
+        for (string word : sub)
+        {
+            prompt += word + " ";
+        }
+        state.prompt(prompt);
     }
-    state.prompt(prompt);
-  }
 }
 
 void fn_pwd(inode_state &state, const wordvec &words)
 {
-  DEBUGF('c', state);
-  DEBUGF('c', words);
-  auto cwdPtr = state.cwd();
-  auto cwdCts = cwdPtr->getContents();
-  if (cwdCts->getDirent(".") == cwdCts->getDirent(".."))
-  {
-    cout << "/" << endl;
-  }
-  else
-  {
-    cwdCts->pwd();
-    cout << endl;
-  }
+    DEBUGF('c', state);
+    DEBUGF('c', words);
+    auto cwdPtr = state.cwd();
+    auto cwdCts = cwdPtr->getContents();
+    if (cwdCts->getDirent(".") == cwdCts->getDirent(".."))
+    {
+        cout << "/" << endl;
+    }
+    else
+    {
+        cwdCts->pwd();
+        cout << endl;
+    }
 }
 
 void fn_rm(inode_state &state, const wordvec &words)
 {
-  DEBUGF('c', state);
-  DEBUGF('c', words);
-  auto cwdPtr = state.cwd();
-  auto cwdCts = cwdPtr->getContents();
-  cwdCts->remove(words.at(1));
+    DEBUGF('c', state);
+    DEBUGF('c', words);
+    auto cwdPtr = state.cwd();
+    auto cwdCts = cwdPtr->getContents();
+
+    if (cwdCts->getDirent(words.at(1))->getContents()->type() == "dir")
+    {
+        cerr << "rm: " << words.at(1) << ": must be a file." << endl;
+        exec::status(1);
+        return;
+    }
+    cwdCts->remove(words.at(1));
 }
 
 void fn_rmr(inode_state &state, const wordvec &words)
 {
-  DEBUGF('c', state);
-  DEBUGF('c', words);
+    DEBUGF('c', state);
+    DEBUGF('c', words);
+    auto cwdPtr = state.cwd();
+    auto cwdCts = cwdPtr->getContents();
+    if (cwdCts->getDirent(words.at(1))->getContents()->type() == "file")
+    {
+        cerr << "rm: " << words.at(1) << ": must be a directory." << endl;
+        exec::status(1);
+        return;
+    }
+    cwdCts->remove(words.at(1));
 }
--- Lab2/file_sys.cpp
+++ Lab2/file_sys.cpp
@@ -13,28 +13,28 @@ int inode::next_inode_nr{1};
 
 struct file_type_hash
 {
-  size_t operator()(file_type type) const
-  {
-    return static_cast<size_t>(type);
-  }
+    size_t operator()(file_type type) const
+    {
+        return static_cast<size_t>(type);
+    }
 };
 
 ostream &operator<<(ostream &out, file_type type)
 {
-  static unordered_map<file_type, string, file_type_hash> hash{
-      {file_type::PLAIN_TYPE, "PLAIN_TYPE"},
-      {file_type::DIRECTORY_TYPE, "DIRECTORY_TYPE"},
-  };
-  return out << hash[type];
+    static unordered_map<file_type, string, file_type_hash> hash{
+        {file_type::PLAIN_TYPE, "PLAIN_TYPE"},
+        {file_type::DIRECTORY_TYPE, "DIRECTORY_TYPE"},
+    };
+    return out << hash[type];
 }
 
 inode_state::inode_state()
 {
-  root = make_shared<inode>(file_type::DIRECTORY_TYPE, "");
-  root->contents->init_dirents(root, root);
-  cwd_ = root;
-  DEBUGF('i', "root = " << root << ", cwd = " << cwd()
-                        << ", prompt = \"" << prompt() << "\"");
+    root = make_shared<inode>(file_type::DIRECTORY_TYPE, "");
+    root->contents->init_dirents(root, root);
+    cwd_ = root;
+    DEBUGF('i', "root = " << root << ", cwd = " << cwd()
+                          << ", prompt = \"" << prompt() << "\"");
 }
 
 const inode_ptr &inode_state::cwd() const { return cwd_; }
@@ -44,30 +44,30 @@ void inode_state::prompt(const string &prompt) { prompt_ = prompt; }
 
 ostream &operator<<(ostream &out, const inode_state &state)
 {
-  out << "inode_state: root = " << state.root
-      << ", cwd = " << state.cwd();
-  return out;
+    out << "inode_state: root = " << state.root
+        << ", cwd = " << state.cwd();
+    return out;
 }
 
 inode::inode(file_type type, const string &name) : inode_nr(next_inode_nr++)
 {
-  switch (type)
-  {
-  case file_type::PLAIN_TYPE:
-    contents = make_shared<plain_file>();
-    break;
-  case file_type::DIRECTORY_TYPE:
-    contents = make_shared<directory>();
-    break;
-  }
-  contents->name(name);
-  DEBUGF('i', "inode " << inode_nr << ", type = " << type);
+    switch (type)
+    {
+    case file_type::PLAIN_TYPE:
+        contents = make_shared<plain_file>();
+        break;
+    case file_type::DIRECTORY_TYPE:
+        contents = make_shared<directory>();
+        break;
+    }
+    contents->name(name);
+    DEBUGF('i', "inode " << inode_nr << ", type = " << type);
 }
 
 int inode::get_inode_nr() const
 {
-  DEBUGF('i', "inode = " << inode_nr);
-  return inode_nr;
+    DEBUGF('i', "inode = " << inode_nr);
+    return inode_nr;
 }
 
 file_error::file_error(const string &what) : runtime_error(what)
@@ -77,192 +77,191 @@ file_error::file_error(const string &what) : runtime_error(what)
 const wordvec &base_file::readfile() const
 {
 
-  throw file_error("is a " + error_file_type());
+    throw file_error("is a " + error_file_type());
 }
 
 void base_file::writefile(const wordvec &)
 {
-  throw file_error("is a " + error_file_type());
+    throw file_error("is a " + error_file_type());
 }
 
 void base_file::remove(const string &)
 {
-  throw file_error("is a " + error_file_type());
+    throw file_error("is a " + error_file_type());
 }
 
 inode_ptr base_file::mkdir(const string &)
 {
-  throw file_error("is a " + error_file_type());
+    throw file_error("is a " + error_file_type());
 }
 
 inode_ptr base_file::mkfile(const string &)
 {
-  throw file_error("is a " + error_file_type());
+    throw file_error("is a " + error_file_type());
 }
 
 void base_file::init_dirents(inode_ptr, inode_ptr)
 {
-  throw file_error("is a " + error_file_type());
+    throw file_error("is a " + error_file_type());
 }
 
 inode_ptr base_file::getDirent(const string &)
 {
-  throw file_error("is a " + error_file_type());
+    throw file_error("is a " + error_file_type());
 }
 
 void base_file::pwd()
 {
-  throw file_error("is a " + error_file_type());
+    throw file_error("is a " + error_file_type());
 }
 
 void base_file::name(const string name)
 {
-  name_ = name;
+    name_ = name;
 }
 
 string base_file::name()
 {
-  return name_;
+    return name_;
 }
 
 void base_file::ls()
 {
-  cout << name_ << endl;
+    cout << name_ << endl;
 }
 
 void base_file::lsr()
 {
-  cout << name_ << endl;
+    cout << name_ << endl;
 }
 
 size_t plain_file::size() const
 {
-  size_t size{0};
-  DEBUGF('i', "size = " << size);
-  return size;
+    size_t size{0};
+    DEBUGF('i', "size = " << size);
+    return size;
 }
 
 const wordvec &plain_file::readfile() const
 {
-  DEBUGF('i', data);
-  return data;
+    DEBUGF('i', data);
+    return data;
 }
 
 void plain_file::writefile(const wordvec &words)
 {
-  DEBUGF('i', words);
-  data = words;
+    DEBUGF('i', words);
+    data = words;
 }
 
 void plain_file::ls()
 {
-  cout << name_ << endl;
+    cout << name_ << endl;
 }
 
 void plain_file::lsr()
 {
-  cout << name_ << endl;
+    cout << name_ << endl;
 }
 
 size_t directory::size() const
 {
-  size_t size{dirents.size()};
-  DEBUGF('i', "size = " << size);
-  return size;
+    size_t size{dirents.size()};
+    DEBUGF('i', "size = " << size);
+    return size;
 }
 
 void directory::init_dirents(inode_ptr parent, inode_ptr self)
 {
-  dirents.insert({".", self});
-  dirents.insert({"..", parent});
+    dirents.insert({".", self});
+    dirents.insert({"..", parent});
 }
 
 void directory::remove(const string &filename)
 {
-  DEBUGF('i', filename);
-  // auto file = dirents.at(filename);
-  dirents.erase(filename);
+    DEBUGF('i', filename);
+    dirents.erase(filename);
 }
 
 inode_ptr directory::mkdir(const string &dirname)
 {
-  DEBUGF('i', dirname);
-  auto dir = make_shared<inode>(file_type::DIRECTORY_TYPE, dirname);
-  dir->getContents()->init_dirents(dirents.find(".")->second, dir);
-  dirents.insert({dirname, dir});
-  return dir;
+    DEBUGF('i', dirname);
+    auto dir = make_shared<inode>(file_type::DIRECTORY_TYPE, dirname);
+    dir->getContents()->init_dirents(dirents.find(".")->second, dir);
+    dirents.insert({dirname, dir});
+    return dir;
 }
 
 inode_ptr directory::mkfile(const string &filename)
 {
-  DEBUGF('i', filename);
-  auto file = make_shared<inode>(file_type::PLAIN_TYPE, filename);
-  dirents.insert({filename, file});
-  return file;
+    DEBUGF('i', filename);
+    auto file = make_shared<inode>(file_type::PLAIN_TYPE, filename);
+    dirents.insert({filename, file});
+    return file;
 }
 
 inode_ptr directory::getDirent(const string &name)
 {
-  return dirents.find(name)->second;
+    return dirents.find(name)->second;
 }
 
 void directory::ls()
 {
-  cout << "/" << name_ << ":" << endl;
-  for (auto dirent : dirents)
-  {
-    cout << "\t";
-    cout << dirent.second->get_inode_nr() << "  ";
-    cout << dirent.second->getContents()->size() << "  ";
-    cout << dirent.first;
-    if (dirent.second->getContents()->type() == "dir")
+    cout << "/" << name_ << ":" << endl;
+    for (auto dirent : dirents)
     {
-      cout << "/";
+        cout << "\t";
+        cout << dirent.second->get_inode_nr() << "  ";
+        cout << dirent.second->getContents()->size() << "  ";
+        cout << dirent.first;
+        if (dirent.second->getContents()->type() == "dir")
+        {
+            cout << "/";
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
 }
 
 void directory::pwd()
 {
-  wordvec pathname;
-  inode_ptr parent = dirents.find(".")->second;
-  while (parent->getContents()->name() != "")
-  {
-    pathname.insert(pathname.begin(), parent->getContents()->name());
-    parent = parent->getContents()->getDirent("..");
-  }
-  for (const string &dirName : pathname)
-  {
-    cout << "/" << dirName;
-  }
+    wordvec pathname;
+    inode_ptr parent = dirents.find(".")->second;
+    while (parent->getContents()->name() != "")
+    {
+        pathname.insert(pathname.begin(), parent->getContents()->name());
+        parent = parent->getContents()->getDirent("..");
+    }
+    for (const string &dirName : pathname)
+    {
+        cout << "/" << dirName;
+    }
 }
 
 void directory::lsr()
 {
-  cout << "/" << name_ << ":" << endl;
-  for (auto dirent : dirents)
-  {
-    cout << "\t";
-    cout << dirent.second->get_inode_nr() << "  ";
-    cout << dirent.second->getContents()->size() << "  ";
-    cout << dirent.first;
-    if (dirent.second->getContents()->type() == "dir")
+    cout << "/" << name_ << ":" << endl;
+    for (auto dirent : dirents)
     {
-      cout << "/";
+        cout << "\t";
+        cout << dirent.second->get_inode_nr() << "  ";
+        cout << dirent.second->getContents()->size() << "  ";
+        cout << dirent.first;
+        if (dirent.second->getContents()->type() == "dir")
+        {
+            cout << "/";
+        }
+        if (dirent != *dirents.end())
+        {
+            cout << endl;
+        }
     }
-    if (dirent != *dirents.end())
+    for (auto dirent : dirents)
     {
-      cout << endl;
+        if (dirent.second->getContents()->type() == "dir" &&
+            dirent.first != "." && dirent.first != "..")
+        {
+            pwd();
+            dirent.second->getContents()->lsr();
+        }
     }
-  }
-  for (auto dirent : dirents)
-  {
-    if (dirent.second->getContents()->type() == "dir" &&
-        dirent.first != "." && dirent.first != "..")
-    {
-      pwd();
-      dirent.second->getContents()->lsr();
-    }
-  }
 }
